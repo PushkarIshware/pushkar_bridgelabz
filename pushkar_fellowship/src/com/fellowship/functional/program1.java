91ea647eaef801a7797926fd1272fd8c5bf937ac
pushkar <pushkar.ishware@gmail.com>
2018-12-13 09:35:14 +0530

    added

---
+++ pushkar_fellowship/src/com/fellowship/functional/program1.java
@@ -0,0 +1,30 @@
+package com.fellowship.functional;
+
+import java.util.Scanner;
+
+import com.fellowship.utilities.*;
+
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s= "hello , how are you ?";
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		if ((name.length()) < 3)
+		{
+			System.out.println("name should be 3 or more letters");
+		}
+			else
+			{
+			System.out.println("ehahaha");
+			}
+		//System.out.println("fdgkjighd");
+			
+		
+		
+				
+	}
+
+}
---
+++ pushkar_fellowship/src/com/fellowship/utilities/utilities.java
@@ -0,0 +1,8 @@
+package com.fellowship.utilities;
+
+import java.util.Scanner;
+
+public class utilities {
+	 Scanner sc = new Scanner(System.in);
+	
+}
